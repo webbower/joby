d5ea1f9c52b9a8249b6ae10eef7a174db40fe2b6
Matt Bower <matt@webbower.com>
2024-09-30 22:37:46 -0700

    Add column sort to `<KanbanBoard>` to sort by Priority

--- src/feat/Kanban/ui/KanbanBoard.tsx
+++ src/feat/Kanban/ui/KanbanBoard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import clsx from 'clsx';
 
 import { LoadingText, Stack } from '~/ui/Layout/mod';
@@ -20,33 +21,71 @@ export type KanbanBoardProps = {
 	onItemClick?: (id: string) => void;
 };
 
-export const KanbanBoard = ({
-	loading = false,
-	cards = [],
-	onItemClick = () => {},
-}: KanbanBoardProps) =>
+export const KanbanBoard = ({ loading = false, cards = [], onItemClick }: KanbanBoardProps) =>
 	loading ? (
 		<LoadingText center color="light" />
 	) : (
 		<div className={css.root}>
-			{cards.map(({ id: columnId, title: columnTitle, items }) => (
-				<section key={columnId} className={clsx(css.column)}>
-					<h2 className={css.columnTitle}>{columnTitle}</h2>
-
-					<Stack className={css.cards}>
-						{items.map(item => (
-							<KanbanCard
-								key={`${item.constructor.name}-${item.id}`}
-								title={item.title}
-								subtitle={item.description}
-								summaryData={getKanbanCardSummaryData(item)}
-								onClick={() => {
-									onItemClick(item.id);
-								}}
-							/>
-						))}
-					</Stack>
-				</section>
+			{cards.map(({ id, title, items }) => (
+				<KanbanColumn key={id} id={id} title={title} items={items} onItemClick={onItemClick} />
 			))}
 		</div>
 	);
+
+type KanbanColumnProps = KanbanCardsGroup & {
+	onItemClick: KanbanBoardProps['onItemClick'];
+};
+
+const sortFns: Record<string, (a: ProductOrder, b: ProductOrder) => number> = {
+	priority: (a, b) => Number(a.priority) - Number(b.priority),
+};
+
+const getOrderedItems = (
+	sortBy: string,
+	items: KanbanCardsGroup['items'],
+): KanbanCardsGroup['items'] => {
+	const sortFn = sortFns[sortBy];
+	if (!sortBy || !sortFn) {
+		return items;
+	}
+
+	return [...items].sort(sortFn);
+};
+
+const KanbanColumn = ({ id, title, items, onItemClick = () => {} }: KanbanColumnProps) => {
+	const [sortBy, setSortBy] = useState('');
+
+	return (
+		<section className={clsx(css.column)}>
+			<header className={css.columnHead}>
+				<h2 className={css.columnTitle}>{title}</h2>
+
+				<div className={css.sort}>
+					<select
+						className={css.sortOptions}
+						onChange={ev => {
+							setSortBy(ev.target.value);
+						}}
+					>
+						<option value="">Sort by</option>
+						<option value="priority">Priority</option>
+					</select>
+				</div>
+			</header>
+
+			<Stack className={css.cards}>
+				{getOrderedItems(sortBy, items).map(item => (
+					<KanbanCard
+						key={`${item.constructor.name}-${item.id}`}
+						title={item.title}
+						subtitle={item.description}
+						summaryData={getKanbanCardSummaryData(item)}
+						onClick={() => {
+							onItemClick(item.id);
+						}}
+					/>
+				))}
+			</Stack>
+		</section>
+	);
+};
